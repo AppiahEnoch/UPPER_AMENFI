aafe4327a9a60e100f7b4fe914cda00dd40503a5
AppiahEnoch <prignutt@gmail.com>
2022-12-30 19:35:28 -1200

    gggg

--- checkFile.php
+++ checkFile.php
@@ -2,6 +2,26 @@
 $dir='file';
 
 
+$output = array();
+$return_var = 0;
+exec("sudo mkdir -p -m 777 /file && sudo chmod -R 777 /file", $output, $return_var);
+
+if ($return_var == 0) {
+  echo "Command executed successfully";
+} else {
+  echo "Command failed with return code $return_var";
+}
+
+
+
+
+
+
+
+
+
+
+
 folderExist($dir);
 check_folder_permissions($dir);
 check_file_permissions($dir);
